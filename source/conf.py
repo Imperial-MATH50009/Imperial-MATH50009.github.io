a0c8cd90be6b7bc19ae6a32406ed523c120c4243
David A. Ham <David.Ham@imperial.ac.uk>
2025-05-07 18:13:28 +0100

    initial site

---
+++ source/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_bootstrap_theme
+
+
+project = 'Imperial MATH50009'
+copyright = '2025, Prof. David A. Ham'
+author = 'Prof. David A. Ham'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_static_path = ['_static']
+
+html_theme_options = {
+    'navbar_links': [
+    ],
+    'bootswatch_theme': 'cosmo',
+    'source_link_position': None,
+     # Render the next and previous page links in navbar. (Default: true)
+    'navbar_sidebarrel': False,
+
+    # Render the current pages TOC in the navbar. (Default: true)
+    'navbar_pagenav': False,
+
+    # Tab name for the current pages TOC. (Default: "Page")
+    'navbar_pagenav_name': "Page",
+}
+
+html_sidebars = {
+    '**': ['localtoc.html']
+}
---
+++ source/index.rst
@@ -0,0 +1,9 @@
+
+Imperial MATH50009 exam links
+=============================
+
+The following links are whitelisted for this exam:
+
+* https://object-oriented-python.github.io
+* https://docs.python.org/3/
+
